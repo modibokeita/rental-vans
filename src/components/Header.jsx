bd08fb49e1fa93e21fac59aed4d0f33d10420a82
Modibo Keita <modibokeita9390@gmail.com>
2025-08-07 00:54:04 +0300

    creating compenets folder with layout component

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+export default function Header() {
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+  return (
+    <header>
+        <Link className='site-logo' to="/">#VanLife</Link>
+        <div className='nav-links'>
+            <NavLink 
+            to="about"
+            style={({ isActive }) => isActive ? activeStyles : null}
+            >About</NavLink>
+            <NavLink 
+            to="vans"
+            style={({ isActive }) => isActive ? activeStyles : null}
+            >Vans</NavLink>
+        </div>
+    </header>
+  )
+}
