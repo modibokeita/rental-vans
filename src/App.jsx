61b4c20b02e317d84dccf5b2b42cf668fe1c68f8
Modibo Keita <modibokeita9390@gmail.com>
2025-08-07 00:56:42 +0300

    Adding app routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans from "./pages/Vans"
+import Layout from "./components/Layout"
 
 
 import "./server"
 
 function App() {
- 
 
   return (
-    <>
-      <h1>Welcome to My App</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
