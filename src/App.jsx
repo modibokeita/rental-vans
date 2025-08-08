c72043279f9cffe57c919100e21131ec0f330b67
Modibo Keita <modibokeita9390@gmail.com>
2025-08-08 23:32:23 +0300

    updating App component to have host routers and adding some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Vans from "./pages/Vans"
+import Vans from "./pages/Vans/Vans"
+import VansDetails from "./pages/Vans/VansDetails"
 import Layout from "./components/Layout"
-
+import HostLayout from "./components/HostLayout"
+import Income from "./pages/Host/Income"
+import Dashboard from "./pages/Host/Dashboard"
+import Reviews from "./pages/Host/Reviews"
+import HostVans from "./pages/Host/HostVans"
 
 import "./server"
 
@@ -17,6 +22,14 @@ function App() {
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VansDetails />} />
+
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="hostvans" element={<HostVans />} />
+            <Route path="income" element={<Income />} />
+          </Route>
         </Route>
 
       </Routes>
